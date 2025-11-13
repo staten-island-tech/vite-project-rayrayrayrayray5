7e10ee614df85220d2834dcd74c99da48ebebb20
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-11-13 13:58:41 -0500

    kkjlkllo

--- app/src/main.js
+++ app/src/main.js
@@ -1,2 +1,15 @@
 import "./style.css";
 import { setupCounter } from "./counter.js";
+song=[
+    {
+        title:"",
+        length:,
+        cover:
+    },
+    {
+        
+    }
+]
+function inject(item){
+    const container = document.querySelector("#app").innerHTML
+}
