5b41a5bf85a929dfa4b4bb7e8367f4391aceb031
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-11-19 14:00:12 -0500

    nm mghjftjhg
    k,

--- app/src/main.js
+++ app/src/main.js
@@ -1,16 +1,23 @@
 import "./style.css";
-import { setupCounter } from "./counter.js";
-song=[
-    {
-        title:"",
-        artist:""
-        length:,
-        cover:,
-    },
-    {
-        
-    }
-]
-function inject(item){
-    const container = document.querySelector("#app").innerHTML
+song = [
+  {
+    title: "The Power of Madonna",
+    artist: "Devon Hendryx",
+    length: 2,
+    cover: "public/jcw.jpg",
+  },
+  {},
+];
+function inject(item) {
+  const container = document.querySelector(".container");
+  container.insertAdjacementHTML(
+    "afterbegin",
+    `<img src="${item.cover}"> 
+        <h2>"${item.title}"</h2>
+        <h2>"${item.artist}"</h2>
+        <h2>"${item.length}"</h2>`
+  );
 }
+song.forEach((element) => {
+  inject(element);
+});
