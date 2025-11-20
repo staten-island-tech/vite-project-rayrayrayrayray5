44602156d9f624286ce6bd96a34cff6cc6927526
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-11-20 14:00:42 -0500

    chester and aiden wabi sabi

--- app/src/main.js
+++ app/src/main.js
@@ -6,16 +6,22 @@ song = [
     length: 2,
     cover: "public/jcw.jpg",
   },
-  {},
+  {
+    title: "The Power of Madonna",
+    artist: "Devon Hendryx",
+    length: 2,
+    cover: "public/jcw.jpg",
+  },
 ];
 function inject(item) {
-  const container = document.querySelector(".container");
+  const container = document.querySelector("#");
   container.insertAdjacementHTML(
     "afterbegin",
-    `<img src="${item.cover}"> 
+    `<div><img src="${item.cover}"> 
         <h2>"${item.title}"</h2>
         <h2>"${item.artist}"</h2>
-        <h2>"${item.length}"</h2>`
+        <h2>"${item.length}"</h2>
+      </div>`
   );
 }
 song.forEach((element) => {
