3ad66ace0d1b20219415474b0cf55a6b4c0fc628
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-11-24 14:00:33 -0500

    chester

--- app/src/main.js
+++ app/src/main.js
@@ -13,9 +13,9 @@ song = [
     cover: "public/jcw.jpg",
   },
 ];
-function inject(item) {
-  const container = document.querySelector("#app");
-  container.insertAdjacementHTML(
+/* function inject(item) {
+  const container = document.querySelector(".app");
+  container.insertAdjacentHTML(
     "afterbegin",
     `<div><img src="${item.cover}"> 
         <h2>"${item.title}"</h2>
@@ -24,6 +24,25 @@ function inject(item) {
       </div>`
   );
 }
+song.forEach((element) => {
+  inject(element);
+});
+ */
+
+function inject(item) {
+  //query the html where we inject the card
+  const container = document.querySelector(".app");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card" data-id="${item.title}" data-price="${item.length}">
+            <h2 class="card-header">${item.title}</h2>
+            <img class="card-img" src="${item.cover}" alt="${item.title}" />
+            <h3 class="card-price">$${item.length}</h3>
+            <button class="button">Buy Now</button>
+        </div>`
+  );
+}
+
 song.forEach((element) => {
   inject(element);
 });
