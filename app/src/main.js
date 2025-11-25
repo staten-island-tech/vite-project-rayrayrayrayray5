5c7620c3841492615fe3b403a44fb9ef87d5307f
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-11-25 13:59:46 -0500

    -0chester

--- app/src/main.js
+++ app/src/main.js
@@ -1,48 +1,46 @@
 import "./style.css";
-song = [
+let song = [
   {
     title: "The Power of Madonna",
     artist: "Devon Hendryx",
-    length: 2,
-    cover: "public/jcw.jpg",
+    length: "5:36",
+    cover: "/jcw.jpg",
   },
   {
-    title: "The Power of Madonna",
-    artist: "Devon Hendryx",
-    length: 2,
-    cover: "public/jcw.jpg",
+    title: "soulja boy 2009",
+    artist: "Illusion Hills",
+    length: "3:44",
+    cover: "/sb2009.webp",
+  },
+  {
+    title: "BREAKUP",
+    artist: "Tiffany Day",
+    length: "3:44",
+    cover: "/breakup.png",
   },
 ];
-/* function inject(item) {
+
+function inject(item) {
   const container = document.querySelector(".app");
   container.insertAdjacentHTML(
     "afterbegin",
     `<div><img src="${item.cover}"> 
-        <h2>"${item.title}"</h2>
-        <h2>"${item.artist}"</h2>
-        <h2>"${item.length}"</h2>
+        <h2>${item.title}</h2>
+        <h2>${item.artist}</h2>
+        <h2>${item.length}</h2>
       </div>`
   );
 }
 song.forEach((element) => {
   inject(element);
 });
- */
 
-function inject(item) {
-  //query the html where we inject the card
-  const container = document.querySelector(".app");
-  container.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="card" data-id="${item.title}" data-price="${item.length}">
-            <h2 class="card-header">${item.title}</h2>
-            <img class="card-img" src="${item.cover}" alt="${item.title}" />
-            <h3 class="card-price">$${item.length}</h3>
-            <button class="button">Buy Now</button>
-        </div>`
-  );
-}
-
-song.forEach((element) => {
-  inject(element);
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
 });
