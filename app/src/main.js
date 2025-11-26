5147a86ff3eb51d5bed4edabdcb6b50d19b2a617
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-11-26 14:00:47 -0500

    aiden.liu24s password is 1

--- app/src/main.js
+++ app/src/main.js
@@ -18,16 +18,58 @@ let song = [
     length: "3:44",
     cover: "/breakup.png",
   },
+  {
+    title: "Baby I'm Bleeding",
+    artist: "JPEGMAFIA",
+    length: "2:32",
+    cover: "/babyimbleeding.png",
+  },
+  {
+    title: "We Don't Leave the House",
+    artist: "glaive",
+    length: "2:25",
+    cover: "/yall.png",
+  },
+  {
+    title: "MONDAY",
+    artist: "Quadeca",
+    length: "4:03",
+    cover: "/monday.webp",
+  },
+  {
+    title: "It's You",
+    artist: "Ninajirachi, daine",
+    length: "2:49",
+    cover: "/ilmc.jpg",
+  },
+  {
+    title: "PIXELATED KISSES",
+    artist: "Joji",
+    length: "1:50",
+    cover: "/pixelatedkisses.webp",
+  },
+  {
+    title: "BLEACH",
+    artist: "BROCKHAMPTON",
+    length: "4:33",
+    cover: "/sat3.png",
+  },
+  {
+    title: "Stateside",
+    artist: "PinkPantheress",
+    length: "2:48",
+    cover: "/stateside.webp",
+  },
 ];
 
 function inject(item) {
   const container = document.querySelector(".app");
   container.insertAdjacentHTML(
     "afterbegin",
-    `<div><img src="${item.cover}"> 
+    `<div><img class="png" src="${item.cover}"> 
         <h2>${item.title}</h2>
         <h2>${item.artist}</h2>
-        <h2>${item.length}</h2>
+        <h2 >${item.length}</h2>
       </div>`
   );
 }
